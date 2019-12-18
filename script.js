94de3e0d150f7e01e7b567fe07dd1d60a2e26d4e
Ben Hernandez <benjammin234@gmail.com>
2019-12-17 17:06:25 -0800

    adjusted offset in js to fix navbar covering content

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $('a[href^="#"]').on('click', function(event) {
     if( target.length ) {
         event.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top - 100
         }, 1000);
     }
 });
@@ -12,7 +12,7 @@ $('a[href^="#"]').on('click', function(event) {
 function hamburger() {
     let menu = document.getElementById('ham-menu');
     if (menu.className === 'scroll-links') {
-        menu.className += ' responsive';
+        menu.className += 'responsive';
     } else {
         menu.className = 'scroll-links';
     }
